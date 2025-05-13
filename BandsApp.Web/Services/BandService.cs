b1a120122f2b061f1a8a5613529cdfdd18818cd5
Hamredahl <JHamredahl@gmail.com>
2025-05-13 13:41:35 +0200

    Small design fixes

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -6,7 +6,7 @@ public class BandService
 {
     private List<Band> bands = [
             new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group.", Image = "Jackson5_500px.png"},
-            new Band { Id = 1, Name = "Baby Metal", Description = "A J-Metal band.", Image = "BABYMETAL_500px.png"},
+            new Band { Id = 1, Name = "BABYMETAL", Description = "A J-Metal band.", Image = "BABYMETAL_500px.png"},
             new Band { Id = 2, Name = "NWA", Description = "Straight Outta Compton.", Image = "NWA_500px.png"}
         ];
 
