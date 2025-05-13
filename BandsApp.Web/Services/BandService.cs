bd06e2f1cab2965c4ee735c62023a4a4788cabf3
Hamredahl <JHamredahl@gmail.com>
2025-05-13 13:50:47 +0200

    Added Albums field to Band

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -5,9 +5,9 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     private List<Band> bands = [
-            new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group.", Image = "Jackson5_500px.png"},
-            new Band { Id = 1, Name = "BABYMETAL", Description = "A J-Metal band.", Image = "BABYMETAL_500px.png"},
-            new Band { Id = 2, Name = "NWA", Description = "Straight Outta Compton.", Image = "NWA_500px.png"}
+            new Band { Id = 0, Name = "Jackson 5", Description = "Jackson 5 are an American pop band composed of members of the Jackson family. The group was formed in Gary, Indiana in 1964, and originally consisted of brothers Jackie, Tito, Jermaine, Marlon, and Michael. The group were among the first African American performers to attain a crossover following.", Image = "Jackson5_500px.png"},
+            new Band { Id = 1, Name = "BABYMETAL", Description = "BABYMETAL is a Japanese kawaii metal band consisting of Suzuka Nakamoto as Su-metal, Moa Kikuchi as Moametal and Momoko Okazaki as Momometal. Their vocals are backed by heavy metal instrumentation, performed by a group of session musicians known as the Kami Band at performances.", Image = "BABYMETAL_500px.png"},
+            new Band { Id = 2, Name = "NWA", Description = "NWA was an American hip hop group formed in Compton, California in 1987. Among the earliest and most significant figures of the gangsta rap subgenre, the group is widely considered one of the greatest and most influential acts in hip hop music.", Image = "NWA_500px.png"}
         ];
 
     public Band? GetBandById(int Id) => bands
