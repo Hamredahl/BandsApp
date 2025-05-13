bdf1e14f0f93f678aa81450a970e95159e7d0362
zhelyazkovse <zhelyazkovse@gmail.com>
2025-05-13 11:50:03 +0200

    Added 3rd band

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -7,7 +7,7 @@ public class BandService
     private List<Band> bands = [
             new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group."},
             new Band { Id = 1, Name = "Baby Metal", Description = "A J-Metal band."},
-            new Band { Id = 2, Name = "", Description = ""}
+            new Band { Id = 2, Name = "NWA", Description = "Straight Outta Compton."}
         ];
 
     public Band? GetBandById(int Id) => bands
