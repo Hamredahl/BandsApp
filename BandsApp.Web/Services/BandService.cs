465571f7a0dc3ff936887a48a63e9370f65c0e65
Hamredahl <JHamredahl@gmail.com>
2025-05-13 13:37:27 +0200

    Added image functionality to details and bandservice

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -5,9 +5,9 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     private List<Band> bands = [
-            new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group."},
-            new Band { Id = 1, Name = "Baby Metal", Description = "A J-Metal band."},
-            new Band { Id = 2, Name = "NWA", Description = "Straight Outta Compton."}
+            new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group.", Image = "Jackson5_500px.png"},
+            new Band { Id = 1, Name = "Baby Metal", Description = "A J-Metal band.", Image = "BABYMETAL_500px.png"},
+            new Band { Id = 2, Name = "NWA", Description = "Straight Outta Compton.", Image = "NWA_500px.png"}
         ];
 
     public Band? GetBandById(int Id) => bands
