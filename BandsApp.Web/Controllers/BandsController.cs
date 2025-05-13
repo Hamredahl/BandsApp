e2e0bc6c29d9a5cbf708bb050615214c73055d95
Hamredahl <JHamredahl@gmail.com>
2025-05-13 11:25:28 +0200

    Added Controller method for empty url call.

---
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,16 @@
+﻿using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Controllers;
+
+public class BandsController : Controller
+{
+    BandService bandsService = new BandService();
+
+    [Route("")]
+    public IActionResult Index()
+    {
+        var model = bandsService.GetAllBands();
+        return View(model);
+    }
+}
