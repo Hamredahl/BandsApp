e7a6729cb4f5101b9039a4b4fc7ab99058c6e03b
Hamredahl <JHamredahl@gmail.com>
2025-05-13 11:16:27 +0200

    Added private list bands to services.

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -1,6 +1,13 @@
-﻿namespace BandsApp.Web.Services
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services;
+
+public class BandService
 {
-    public class BandService
-    {
-    }
+    private List<Band> bands = [
+            new Band { Id = 0, Name = "Jackson 5", Description = "A pop-music group."},
+            new Band { Id = 1, Name = "Baby Metal", Description = "A J-Metal band."},
+            new Band { Id = 2, Name = "", Description = ""}
+        ];
+
 }
